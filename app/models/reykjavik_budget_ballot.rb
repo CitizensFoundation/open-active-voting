522f47b501a28015ec2b8184be85f39e5ca7392c
Robert Bjarnason <rbjarnason>
2012-02-01 20:33:26 +0000

    Merge from dev

--- app/models/reykjavik_budget_ballot.rb
+++ app/models/reykjavik_budget_ballot.rb
@@ -7,16 +7,13 @@ class ReykjavikBudgetBallot
   def initialize
     @construction_priorities = []
     @maintenance_priorities = []
-    @construction_priorities << {:id=>1, :name=>"Nýframkvæmd 1"}
-    @construction_priorities << {:id=>2, :name=>"Nýframkvæmd 2"}
-    @construction_priorities << {:id=>3, :name=>"Nýframkvæmd 3"}
-    @construction_priorities << {:id=>4, :name=>"Nýframkvæmd 4"}
-    @construction_priorities << {:id=>5, :name=>"Nýframkvæmd 5"}
+    count = 0
+    13.times do
+      @construction_priorities << {:id=>count+=1, :name=>"Nýframkvæmd #{count}"}
+    end
 
-    @maintenance_priorities << {:id=>1, :name=>"Viðhaldsverkefni 1"}
-    @maintenance_priorities << {:id=>2, :name=>"Viðhaldsverkefni 2"}
-    @maintenance_priorities << {:id=>3, :name=>"Viðhaldsverkefni 3"}
-    @maintenance_priorities << {:id=>4, :name=>"Viðhaldsverkefni 4"}
-    @maintenance_priorities << {:id=>5, :name=>"Viðhaldsverkefni 5"}
+    13.times do
+      @maintenance_priorities << {:id=>count+=1, :name=>"Viðhaldsverkefni #{count}"}
+    end
   end
 end
--- test/integration/votes_test.rb
+++ test/integration/votes_test.rb
@@ -4,17 +4,28 @@ require "watir-webdriver"
 require 'test_helper'
 
 class VoteThroughBrowsers < ActionController::IntegrationTest
-  test "test_vote_through_chrome" do
+  test "test_vote_through_firefox" do
     db_config = YAML::load(File.read(Rails.root.join("config","database.yml")))
     @browser ||= Watir::Browser.new :firefox
-    @browser.goto "http://robert:#{db_config[Rails.env]['http_test_password']}@ktest.betrireykjavik.is/votes/get_ballot"
-    @browser.checkbox(:value => '1').set
-    @browser.checkbox(:value => '2').set
-    @browser.checkbox(:value => '3').set
-    @browser.checkbox(:value => '4').set
-    @browser.button.click
-    @browser.div(:id => "success_message").wait_until_present
-    assert @browser.div(:id => "success_message") == "success"
+
+    5.times do
+      @browser.goto "http://ktest.betrireykjavik.is/votes/get_ballot"
+      setup_checkboxes
+      @browser.button.click
+      @browser.div(:id => "success_message").wait_until_present
+      success_value = @browser.div(:id => "success_message").value
+      Rails.logger.debug success_value
+      assert @browser.text.include?("success")
+    end
+  end
+
+  def teardown
     @browser.close
+  end
+
+  def setup_checkboxes
+    5.times do
+      @browser.checkbox(:value => "#{rand(5+1)}").set
+    end
   end
 end
