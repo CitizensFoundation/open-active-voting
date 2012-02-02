b7bb2b2a93e282a3eceedbe906904c4629b69f22
Robert Bjarnason <rbjarnason>
2012-02-02 15:31:13 +0000

    Merge from dev

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,8 @@
+require 'digest/sha1'
+require 'nokogiri'
+require 'soap/rpc/driver'
+require 'soap/wsdlDriver'
+
 class VotesController < ApplicationController
 
   after_filter :log_session_id
@@ -11,11 +16,12 @@ class VotesController < ApplicationController
   end
 
   def authenticate_from_island_is
-    if Vote.authenticate_from_island_is(params[:token],request)
+    if authenticate_from_island_is(params[:token],request)
       session[:have_authenticated_and_been_approved]=true
       redirect_to :action=>:get_ballot
     else
       Rails.logger.error("No identity from island.is for session id: #{request.session_options[:id]}")
+      flash[:notice]="Authentication Error!!!"
       redirect_to :action=>:authentication_error
     end
   end
@@ -75,6 +81,32 @@ class VotesController < ApplicationController
     end
     respond_to do |format|
       format.json { render :json => response }
+    end
+  end
+
+  private
+
+  def authenticate_from_island_is(token,request)
+    begin
+      soap_url = "https://egov.webservice.is/sst/runtime.asvc/com.actional.soapstation.eGOVDKM_AuthConsumer.AccessPoint?WSDL"
+      soap = SOAP::WSDLDriverFactory.new(soap_url).create_rpc_driver
+      soap.options["protocol.http.basic_auth"] << [soap_url,DB_CONFIG[RAILS_ENV]['rsk_soap_username'],DB_CONFIG[RAILS_ENV]['rsk_soap_password']]
+      @response = soap.generateSAMLFromToken(token,:token => token, :ipAddress=>request.remote_ip)
+
+      if @response and @response[0] and @response[0].message="Success"
+        elements = Nokogiri.parse(@response[1])
+        name = elements.root.xpath("//blarg:NameIdentifier", {'blarg' => 'urn:oasis:names:tc:SAML:1.0:assertion'}).first.text
+        national_identity_hash = elements.root.xpath("//blarg:Attribute[@AttributeName='SSN']", {'blarg' => 'urn:oasis:names:tc:SAML:1.0:assertion'}).text
+      else
+        raise "Message was not a success #{response.inspect}"
+      end
+      Rails.cache.write(request.session_options[:id],national_identity_hash)
+      Rails.logger.info("Authentication successful for #{national_identity_hash} #{response.inspect}")
+      return true
+    rescue  => ex
+      Rails.logger.error(ex.to_s+"\n\n"+ex.backtrace.to_s)
+      Rails.logger.error(@response.inspect)
+      return false
     end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,26 +1,2 @@
 class Vote < ActiveRecord::Base
-
-  def self.authenticate_from_island_is(token,request)
-    begin
-      soap_url = "https://egov.webservice.is/sst/runtime.asvc/com.actional.soapstation.eGOVDKM_AuthConsumer.AccessPoint?WSDL"
-      soap = SOAP::WSDLDriverFactory.new(soap_url).create_rpc_driver
-      soap.options["protocol.http.basic_auth"] << [soap_url,DB_CONFIG[RAILS_ENV]['rsk_soap_username'],DB_CONFIG[RAILS_ENV]['rsk_soap_password']]
-      @response = soap.generateSAMLFromToken(token,:token => token, :ipAddress=>request.remote_ip)
-
-      if @response and @response[0] and @response[0].message="Success"
-        elements = Nokogiri.parse(@response[1])
-        name = elements.root.xpath("//blarg:NameIdentifier", {'blarg' => 'urn:oasis:names:tc:SAML:1.0:assertion'}).first.text
-        national_identity_hash = elements.root.xpath("//blarg:Attribute[@AttributeName='SSN']", {'blarg' => 'urn:oasis:names:tc:SAML:1.0:assertion'}).text
-      else
-        raise "Message was not a success #{response.inspect}"
-      end
-      Rails.cache.write(request.session_options[:id],national_identity_hash)
-      Rails.logger.info("Authentication successful for #{national_identity_hash} #{response.inspect}")
-      return true
-    rescue  => ex
-      Rails.logger.error(ex.to_s+"\n\n"+ex.backtrace.to_s)
-      Rails.logger.error(@response.inspect)
-      return false
-    end
-  end
 end
