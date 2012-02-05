37163bedc4de157e52d6c2a30638431f51190532
Robert Bjarnason <rbjarnason>
2012-02-05 19:18:07 +0000

    merge from dev

--- test/integration/votes_test.rb
+++ test/integration/votes_test.rb
@@ -5,8 +5,8 @@ require 'test_helper'
 
 class VoteThroughBrowsers < ActionController::IntegrationTest
   def setup
-    @max_browsers = 25
-    @max_votes = 125
+    @max_browsers = 4
+    @max_votes = 7
     @db_config = YAML::load(File.read(Rails.root.join("config","database.yml")))
     @neighborhood_ids = [1,2]
     #@neighborhood_ids = [1,2,3,4,5,6,7,8,9,10]
@@ -23,16 +23,16 @@ class VoteThroughBrowsers < ActionController::IntegrationTest
       @browsers << Watir::Browser.new(@browser_types[rand(@browser_types.length)])
     end
     @votes = []
-    @final_votes = Hash.new
-    @neighborhood_ids.each do |neighborhood_id|
-      @final_votes[neighborhood_id] = Hash.new unless @final_votes[neighborhood_id]
-      @browsers.each do |browser|
-        @final_votes[neighborhood_id][browser] = []
-      end
+    @user_browser_votes = Hash.new
+    @browsers.each do |browser|
+      @user_browser_votes[browser] = []
     end
 
     @database_csv_filenames = []
     @test_csv_filenames = []
+
+    FileUtils.rm_rf File.join(Rails.root.join("test","results"))
+    Dir.mkdir(File.join(Rails.root.join("test","results")))
 
     setup_votes
   end
@@ -49,7 +49,7 @@ class VoteThroughBrowsers < ActionController::IntegrationTest
       neighborhood_id = @neighborhood_ids[rand(@neighborhood_ids.length)]
       browser.goto "http://localhost:3000/votes/ballot?neighborhood_id=#{neighborhood_id}"
       setup_checkboxes(browser,vote)
-      @final_votes[neighborhood_id][browser] << get_user_votes(browser)
+      @user_browser_votes[browser] << {:neighborhood_id=>neighborhood_id, :votes=>get_user_votes(browser)}
       browser.button.click
       browser.div(:id => "success_message").wait_until_present
     end
@@ -72,18 +72,18 @@ class VoteThroughBrowsers < ActionController::IntegrationTest
 
   def all_votes
     all_votes = []
-    @final_votes.each do |neighborhood, browsers|
-      browsers.each do |browser, values|
-        all_votes+=values
+    @user_browser_votes.each do |browsers,vote_values|
+      vote_values.each do |vote_value|
+        all_votes<<vote_value[:votes]
       end
     end
-    all_votes
+  all_votes
   end
 
   def get_unique_votes(neighborhood_id)
     unique_votes = []
-    @final_votes[neighborhood_id].each do |browser,values|
-      unique_votes << values.last
+    @user_browser_votes.each do |browsers,vote_values|
+      unique_votes<<vote_values.last[:votes] if vote_values.last[:neighborhood_id] == neighborhood_id
     end
     unique_votes
   end
@@ -91,15 +91,19 @@ class VoteThroughBrowsers < ActionController::IntegrationTest
   def unique_vote_match?
     votes.each do |vote| puts vote.inspect end # DEBUG
     all_passed = true
+    Vote.split_and_generate_final_votes!
     @neighborhood_ids.each do |neighborhood_id|
       puts "NEIGHBORHOOD ID #{neighborhood_id}"
       database_count = ReykjavikBudgetVoteCounting.new(Rails.root.join('test','keys','privkey.pem'))
       @database_csv_filenames << database_count.count_unique_votes(neighborhood_id)
-      puts database_count.inspect
+      #puts database_count.inspect
       test_count = ReykjavikBudgetVoteCounting.new(Rails.root.join('test','keys','privkey.pem'))
       test_count.count_all_test_votes(get_unique_votes(neighborhood_id),neighborhood_id)
       @test_csv_filenames << test_count.write_voting_results_report("test_voting_results.csv")
-      puts test_count.inspect
+      #puts test_count.inspect
+      puts "Test: ct: #{test_count.construction_priority_ids_count} == #{database_count.construction_priority_ids_count}"
+      puts "Test: mt: #{test_count.maintenance_priority_ids_count} == #{database_count.maintenance_priority_ids_count}"
+
       match = (test_count.construction_priority_ids_count == database_count.construction_priority_ids_count &&
                test_count.maintenance_priority_ids_count == database_count.maintenance_priority_ids_count) ? true : false
       unless match
@@ -108,14 +112,13 @@ class VoteThroughBrowsers < ActionController::IntegrationTest
         break
       end
     end
-    puts "NEIGHEND"
     all_passed
   end
 
   def identical_csv_files?
     all_passed = true
     @neighborhood_ids.each_with_index do |neighborhood_id,index|
-       match = File.open(Rails.root.join("results",@database_csv_filenames[index])).read.to_s == File.open(Rails.root.join("results",@test_csv_filenames[index])).read.to_s
+       match = File.open(Rails.root.join("test","results",@database_csv_filenames[index])).read.to_s == File.open(Rails.root.join("test","results",@test_csv_filenames[index])).read.to_s
        unless match
         puts "FAILED"
         all_passed = false
