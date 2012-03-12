01c93e558ad37bf2e1b35d33f3f277849121bdea
Róbert Viðar Bjarnason <robert.bjarnason@gmail.com>
2012-03-12 18:34:48 +0000

    Fix timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < ActionController::Base
       session[:expires_at] = 1.minutes.from_now
 #      session[:expires_at] = 600.hours.from_now
     else
-      session[:expires_at] = 1.minutes.from_now
+      session[:expires_at] = 20.minutes.from_now
     end
   end
 
