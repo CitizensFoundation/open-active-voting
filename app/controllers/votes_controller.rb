f7992b2ce27e6445821cd2d5faf582cf60fa04d5
Róbert Viðar Bjarnason <robert@idea-synergy.com>
2013-03-25 01:35:48 +0000

    Fixes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -271,7 +271,8 @@ class VotesController < ApplicationController
       test_x509_cert = "-----BEGIN CERTIFICATE-----#{text_response[start_token_start+18..end_token_start-8]}-----END CERTIFICATE-----".gsub("\n","").gsub(/\n/,"")
       known_x509_cert_txt = known_x509_cert.to_s.gsub("\n","")
 
-      raise "Failed to verify x509 cert KNOWN #{known_x509_cert_txt} (#{known_x509_cert_txt.size}) TEST #{test_x509_cert} (#{test_x509_cert.size}) " unless known_x509_cert_txt == test_x509_cert
+
+      raise "Failed to verify x509 cert KNOWN #{known_x509_cert_txt} (#{known_x509_cert_txt.size}) |#{known_x509_cert_txt.encoding.name}| TEST #{test_x509_cert} (#{test_x509_cert.size}) |#{test_x509_cert.encoding.name}|" unless known_x509_cert_txt == test_x509_cert
 
       # Write the national identity hash to memcache under our session id
       if national_identity_hash and national_identity_hash!=""
