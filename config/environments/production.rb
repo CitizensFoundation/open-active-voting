112b3869f95d52d2d41922968855c12e528c978e
Róbert Viðar Bjarnason <robert@citizens.is>
2014-01-29 08:31:55 +0000

    Don't startup Rails on precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,5 @@ OpenActiveVoting::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.initialize_on_precompile = false
 end
