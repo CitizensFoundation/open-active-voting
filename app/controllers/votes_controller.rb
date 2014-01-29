1b8599783900ac256e88dd5f3e320acfe06776ad
Róbert Viðar Bjarnason <robert@citizens.is>
2014-01-29 12:27:10 +0000

    Fix

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -239,7 +239,7 @@ class VotesController < ApplicationController
       soap = SOAP::WSDLDriverFactory.new(soap_url).create_rpc_driver
       soap.options["protocol.http.basic_auth"] << [soap_url,@config.rsk_soap_username,@config.rsk_soap_password]
 
-      Rails.logger.info("soap: #{soap.soap}")
+      Rails.logger.info("soap: #{soap}")
 
       # Get SAML response from island.is
       @response = soap.generateElectionSAMLFromToken(:token => token, :ipAddress=>request.remote_ip,
