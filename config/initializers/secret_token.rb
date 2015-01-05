f8005c62855d63e6fe61259ac6ddb12e41d40ffa
Róbert Viðar Bjarnason <robert@citizens.is>
2015-01-05 19:10:37 +0000

    Default secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 if ENV['RAILS_SECRET_TOKEN']
   OpenActiveVoting::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
 else
-  raise "ENV['RAILS_SECRET_TOKEN'] is missing. Add the missing ENV variable."
+ OpenActiveVoting::Application.config.secret_token = "82d58d3dfb91238b495a311eb8539edf5064784f1d58994679db8363ec241c745bef0b446bfe44d66cbf91a2f4e497d8f6b1ef1656e3f405b0d263a9617ac75e"
 end
