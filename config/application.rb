03e52c2d8ab83e260cfe66bd4feb812d6fb756ba
Róbert Viðar Bjarnason <robert@citizens.is>
2016-08-17 12:48:48 +0000

    Auto load libs

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module OpenActiveVotingApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
