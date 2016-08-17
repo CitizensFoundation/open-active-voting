baa88517a0e9e4ec56590cb2597360f7d8b474a8
Róbert Viðar Bjarnason <robert@citizens.is>
2016-08-17 15:11:12 +0000

    Change to SSN

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -177,7 +177,7 @@ class VotesController < ApplicationController
       # Check and see if the response is a success
       if saml_validation_response==true
         parsed = Nokogiri.parse(@response.response.to_s)
-        national_identity_hash = parsed.root.xpath("//blarg:Attribute[@FriendlyName='Kennitala']", {"blarg" => 'urn:oasis:names:tc:SAML:2.0:assertion'}).children[0].text
+        national_identity_hash = parsed.root.xpath("//blarg:Attribute[@FriendlyName='SSN']", {"blarg" => 'urn:oasis:names:tc:SAML:2.0:assertion'}).children[0].text
         assertion_id = parsed.root.xpath("//blarg:Assertion/@ID", {"blarg" => 'urn:oasis:names:tc:SAML:2.0:assertion'}).to_s
         if SamlAssertion.where(:assertion_id=>assertion_id).first
           raise "Duplicate SAML 2 assertion error"
