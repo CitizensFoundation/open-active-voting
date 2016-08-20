a8927b17c8f9ef7e632236a072b8510a2e9c2f79
Róbert Viðar Bjarnason <robert@citizens.is>
2016-08-20 05:31:27 +0000

    New dist

--- public/build/bundled/service-worker.js
+++ public/build/bundled/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","c38c0ee843f05d9d05d3ad4189335381"],["/src/oav-app.html","fbccb9af7f4d772be1f251ca6d537f1a"],["/src/oav-area-ballot/oav-area-ballot.html","c4a74f9a82983c22604224120344acb6"],["/src/oav-select-voting-area/oav-select-voting-area.html","73cdc645e07921673028eb811e6f5e0d"],["/src/oav-voting-completed/oav-voting-completed.html","c01cc05eb16e7583dd0277892543b78d"],["/src/yp-post/yp-post.html","7be845f68ceae0ecbbcc3936aba136ef"]];
+var PrecacheConfig = [["/index.html","0548b413b7272f9fc80ae30bf2ea8478"],["/src/oav-app.html","fbccb9af7f4d772be1f251ca6d537f1a"],["/src/oav-area-ballot/oav-area-ballot.html","17c826a378c500aaaa5513366a2bd6da"],["/src/oav-select-voting-area/oav-select-voting-area.html","73cdc645e07921673028eb811e6f5e0d"],["/src/oav-voting-completed/oav-voting-completed.html","c01cc05eb16e7583dd0277892543b78d"],["/src/yp-post/yp-post.html","7be845f68ceae0ecbbcc3936aba136ef"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
