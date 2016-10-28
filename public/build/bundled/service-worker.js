c3bfd738bc3a9280b12a73cd87402a62716aaa7b
Robert Bjarnason <Robert Bjarnason>
2016-10-28 22:14:44 +0000

    New dist

--- public/build/bundled/service-worker.js
+++ public/build/bundled/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/bower_components/webcomponentsjs/webcomponents-lite.min.js","f04ed23700daeb36f637bfe095960659"],["/index.html","9609f564c41361b94709d3148092e0c6"],["/manifest.json","96cb2afc3238c26dc9b1388704275d2a"],["/src/oav-app.html","7c601c3fb69f29c0130b91b9f345379b"],["/src/oav-area-ballot/oav-area-ballot.html","d739a46ddf67efeb33fae357745d12ce"],["/src/oav-select-voting-area/oav-select-voting-area.html","520d43c57d935f7a116d808842d7705a"],["/src/oav-voting-completed/oav-voting-completed.html","3ed6a7d81ab59db127512567cefad70a"],["/src/yp-post/yp-post.html","62018714896e0d499c8e4e9e9e35127a"]];
+var PrecacheConfig = [["/bower_components/webcomponentsjs/webcomponents-lite.min.js","f04ed23700daeb36f637bfe095960659"],["/index.html","9609f564c41361b94709d3148092e0c6"],["/manifest.json","96cb2afc3238c26dc9b1388704275d2a"],["/src/oav-app.html","57a592b21925ac2dfe52606b39b571b3"],["/src/oav-area-ballot/oav-area-ballot.html","d739a46ddf67efeb33fae357745d12ce"],["/src/oav-select-voting-area/oav-select-voting-area.html","520d43c57d935f7a116d808842d7705a"],["/src/oav-voting-completed/oav-voting-completed.html","3ed6a7d81ab59db127512567cefad70a"],["/src/yp-post/yp-post.html","62018714896e0d499c8e4e9e9e35127a"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
