71378ae54603af7c7601a0fa3cba5093d2faedfc
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-05-04 00:57:55 +0000

    Work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # We do not use this SSL is managed by Nginx on the frontend
+  config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-OpenActiveVotingApp::Application.config.session_store :cookie_store, key: '_open-active-voting_session'
+OpenActiveVotingApp::Application.config.session_store :cookie_store, key: '_open-active-voting_session', ::secure => true
