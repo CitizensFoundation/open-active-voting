2b749f2b8fd5306d9f4e1b32545406dfd74a7609
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-05-04 01:50:14 +0000

    Work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # We do not use this SSL is managed by Nginx on the frontend
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
