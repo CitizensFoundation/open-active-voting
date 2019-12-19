72f4fcdf4cd2730675cf0569782418b59df25726
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-12-19 18:21:46 +0000

    Fix for mobile

--- public/src/components/oav-area-ballot.js
+++ public/src/components/oav-area-ballot.js
@@ -70,7 +70,7 @@ class OavAreaBallot extends PageViewElement {
               <paper-tab>
                 <div ?hidden="${!this.wide}">${this.area.name}</div>
                 <div ?hidden="${this.wide}" class="layout vertical center-center">
-                  <div>${this.area.name}</div>
+                  <div>${this.localize('items_list')}</div>
                 </div>
               </paper-tab>
               <paper-tab ?hidden="${this.configFromServer.client_config.hideLocation}">${this.localize('items_on_map')}</paper-tab>
