32c6552ee57b70f80ec14beda269b25f2c7e4c1f
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-07-08 19:20:03 +0000

    TS upgrade

--- public/web-dev-server.config.mjs
+++ public/web-dev-server.config.mjs
@@ -4,17 +4,35 @@ import proxy from 'koa-proxies';
 
 const commonjs = fromRollup(rollupCommonjs);
 
-export default {
-  mimeTypes: {
-    '**/*.cjs': 'js'
-  },
-  port: 9000,
+// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+
+/** Use Hot Module replacement by adding --hmr to the start command */
+//const hmr = process.argv.includes('--hmr');
+
+export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
+  nodeResolve: true,
+  open: '/',
   middleware: [
     proxy('/votes/', {
       target: 'http://localhost:3012/',
     }),
   ],
+  //watch: !hmr,
+
+  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
+  //esbuildTarget: 'auto',
+
+  /** Set appIndex to enable SPA routing */
+  // appIndex: 'demo/index.html',
+
+  /** Confgure bare import resolve plugin */
+  // nodeResolve: {
+  //   exportConditions: ['browser', 'development']
+  // },
+
   plugins: [
     commonjs(),
   ],
-};
+
+  // See documentation for all available options
+});
