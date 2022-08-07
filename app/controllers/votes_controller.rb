eab9072eb53f703d39392fe3ae883db5b850da62
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-08-07 18:16:42 +0000

    Work on sms login

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -30,6 +30,7 @@ class VotesController < ApplicationController
   # Logout and reset the session
   def logout
     Rails.logger.info("Logout")
+    session[:random_sms_code] = nil
     reset_session
   end
 
@@ -106,6 +107,56 @@ class VotesController < ApplicationController
       else
         raise "Authentication was not a success vote not found for insecure email"
       end
+    else
+      raise "Trying to use insecure email login when not enabled"
+    end
+  end
+
+  # For smaller projects with no access to secure logins and for testing purposes
+  def insecure_sms_login
+    if ENV["INSECURE_SMS_LOGIN_ENABLED"]
+      insecure_sms = params[:insecure_sms]
+      if session[:random_sms_code]==params[:user_sms_code]
+        # Find the previously stored wote from the session id that has not been authenticated before
+        vote = Vote.order("created_at DESC").where(:session_id=>request.session_options[:id].to_s, :saml_assertion_id=>nil).first
+
+        if vote
+          # Create an encrypted checksum
+          encrypted_vote_checksum = Vote.generate_encrypted_checksum(insecure_sms,
+                                          vote.payload_data,vote.client_ip_address,vote.area_id,request.session_options[:id])
+
+          # Update the values for the vote and confirm it as being authenticated
+          vote.encrypted_vote_checksum = encrypted_vote_checksum
+
+          if @config.client_config["allowVotesForAllAreas"] and @config.client_config["allowVotesForAllAreas"]==true
+            insecure_sms += "-" + vote.area_id.to_s
+          end
+
+          vote.user_id_hash = insecure_sms
+          vote.authenticated_at = Time.now
+          vote.user_postcode = postcode
+          vote.saml_assertion_id = -1
+          vote.save
+          respond_to do |format|
+            format.json { render :json => {:ok=>true} }
+          end
+        else
+          raise "Authentication was not a success vote not found for insecure email"
+        end
+      else
+        respond_to do |format|
+          render json: {}, status: :unauthorised
+        end
+    else
+      raise "Trying to use insecure email login when not enabled"
+    end
+  end
+
+  def insecure_sms_login_code
+    if ENV["INSECURE_SMS_LOGIN_ENABLED"]
+      random_sms_code = session[:random_sms_code] = 4.times.map{rand(10)}.join
+      sms_text = @config.client_config["sms_text"].gsub("#SMSCODE#", random_sms_code)
+      # SEND SMS WITH A GET REQUEST
     else
       raise "Trying to use insecure email login when not enabled"
     end
