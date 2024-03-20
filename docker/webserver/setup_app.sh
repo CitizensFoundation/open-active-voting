e55198b1835b802106ecd75b077c79eb582d1582
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-03-20 00:58:22 +0000

    Merge from dev

--- docker/webserver/setup_app.sh
+++ docker/webserver/setup_app.sh
@@ -7,8 +7,8 @@ cd /home/app/oav_website
 
 #rm -r /home/app/oav_website/vendor/bundle
 
-sudo -u app bundle install --deployment --verbose
-sudo -u app bundle exec rake db:migrate RAILS_ENV=production
+sudo -u app bundle install --deployment
+#sudo -u app bundle exec rake db:migrate RAILS_ENV=production
 #sudo -u app bundle exec rake db:seed RAILS_ENV=production
 mkdir -p /var/log/nginx
 
